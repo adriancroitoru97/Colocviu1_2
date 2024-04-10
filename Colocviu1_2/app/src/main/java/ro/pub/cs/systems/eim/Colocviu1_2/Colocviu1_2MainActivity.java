fc2b2b5d882dc893a1a2f55dd2eb6d4c6d2a4ce6
Adrian Croitoru <adrian.croitoru.97@gmail.com>
2024-04-10 08:18:32 +0300

    initial commit

---
+++ Colocviu1_2/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Colocviu1_2MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.Colocviu1_2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Colocviu1_2MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_2_main);
+    }
+}
